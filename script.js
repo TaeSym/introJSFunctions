562a67f2d218ca4d5c67bce7d0211360ee2fa9bb
TaeSym <shauntesymone@gmail.com>
2023-10-11 01:08:36 -0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,20 @@
+var welcome = function (name) {
+  console.log(`Welcome, ${name}. Have a great day!`);
+};
+
+var addTogether = function (num1, num2) {
+  return num1 + num2;
+};
+
+welcome("Vera");
+welcome("Adrian");
+welcome("Aida");
+
+addTogether(13, 72);
+addTogether(36, -2.88);
+
+alert(addTogether(36, -2.88));
+console.log(addTogether(13, 72));
+var lunchForTwo = addTogether(24.56, 18.99);
+
+console.log(lunchForTwo);
